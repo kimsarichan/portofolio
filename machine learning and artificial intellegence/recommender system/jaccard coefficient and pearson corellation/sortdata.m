6ed8e2f3af0e8daac783972a72bf9603f4cae3c3
kimsarichan <sari.rahmawati11@gmail.com>
2017-02-01 15:37:37 +0700

    edit readme

--- machine learning and artificial intellegence/recommender system/jaccard coefficient and pearson corellation/sortdata.m
+++
@@ -1,8 +0,0 @@
-function sortdata()
-pathname = 'data/';
-ratingsfname = 'u.data';
-ratings = load([pathname ratingsfname]);
-%selectedrows = find(ratings(:,1) == uid);
-ys = ratings(1,:);
-fprintf(ys(i,2))
-end
